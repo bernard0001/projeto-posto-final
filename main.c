54909cb37fd9837fb22882f1a505b64dd11cce91
bernard000 <118988204+bernard0001@users.noreply.github.com>
2022-11-24 14:48:43 +0000

    fila e abastecimento

--- main.c
+++ main.c
@@ -1,85 +1,143 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
-  int opcao = 0;
-  float preco;
-  int fila;
-  int carros;
-  char opcao2 =' ' ;
-  float pausa;
+  int opcao = 0, fila, carros, abastecimento, atendrel;
+  float preco, pausa, tanque, gasolina, gasorel, vendasrel;
+  char opcao2 = ' ';
+  preco = 0, carros=0, fila = 0, atendrel = 0, tanque = 200, gasolina = 0, gasorel = 0, vendasrel = 0;
   
-  carros = 0;
-  printf("Seja bem vindo ao programa do programador Bernard Moreno, o mesmo "
-         "possui a intenção de agilizar e melhorar a gestão de postos de "
-         "gasolina através de diversos comandos e funcionalidades utéis.\n\n");
-  printf("Por favor, informe o preço da gasolina se precisar use ponto para "
-         "valores não inteiros:\n");
-  scanf("%f", &preco);
+  while (preco <= 0) {
+    printf("Por favor, informe o preço da gasolina se precisar "
+           "use ponto para valores não inteiros:\n");
+    scanf("%f", &preco);
+  }
   printf(" \n");
-  printf(
-      "Por favor, informe a quantidade de carros que se encontram na fila:\n");
-  scanf("%d", &fila);
-  printf(" \n");
-  while (opcao != 5) {
-    printf("                            =MENU=                           \n");
-    printf(" \n\n");
-    printf(" ");
-    printf("Carros na fila: %d\n", carros);
+  while (fila <= 0) {
+    printf(
+        "Por favor, informe a quantidade de carros máxima que a"
+        " fila terá:\n");
+    scanf("%d", &fila);
     printf(" \n");
-    printf(" (1) Adicionar um carro a fila\n (2) Abastecimento\n (3) Exibir "
-           "carros na fila de espera\n (4) Relatórios\n (5) Encerrar\n");
-    printf("\nPor favor, escolha uma das opções.");
+  }
+  system("clear");
+  while (opcao != 5) { 
+    printf("\n                            =MENU=                           \n");
+    printf(" \n\n\n");
+    printf("Carros na fila: %d\n\n", carros);
+    printf("(1) Adicionar um carro a fila\n"
+           "(2) Abastecimento\n"
+           "(3) Exibir "
+           "carros na fila de espera\n"
+           "(4) Relatórios\n" 
+           "(5) Encerrar\n");
+    printf("\nPor favor, escolha uma das opções.\n");
     scanf("\n%d", &opcao);
     switch (opcao) {
-    case 1:
-      printf("\nCarro adicionado na fila.\n");
-      carros++;
-      break;
-    case 2:
-      break;
-    case 3:
-      break;
-    case 4:
-     while(opcao2!='F'){ 
-       printf("\n A - Quantidade de litros vendida.\n B - Valor total arrecadado"
-             "com as vendas.\n C - Quantidade de carros atendidos.\n D - "
-             "Quantidade de combustível restante no tanque.\n E - Gerar "
-             "arquivo para impressão (com todas as informações de A,B,C e D.\n "
-             "F - Voltar ao menu anterior.\n");
-      printf("\nPor favor escolha uma das opções. ");
-      scanf("\n%c", &opcao2);
-      switch (opcao2){
-        case 'A':
-          printf("\nA quantidade de litros vendida é: XX\n");
-          break;
-          case 'B':
-            printf("\nValor total arrecadado: XX\n");
-          break;
-            case 'C':
-              printf("\nQuantidade de carros atendida é: XX\n");
-            break;
-              case 'D':
-                printf("\nQuantidade de combustível restante no tanque é: XX\n");
-              break;
-                case 'E':
-                  printf("\nArquivo para impressão:\n A - XX.\n B - XX.\n C - XX\n D - XX.\n");
-                break;
-                  case 'F':
-                    printf("\nVoltando ao outro menu.\n");
-                  break;  
-        default:
-          printf("\nOpção fora do escopo.\n");
-        break;
+      case 1:
+      system("clear");
+        if (carros == fila) {
+        printf("\nTamanho de fila máximo atingido.\n");
+        } else {
+        printf("\nCarro adicionado na fila.\n");
+        carros += 1;
         }
-       } 
-       break;
-    case 5:
-      printf("Encerrando programa.");
-      break;
-    default:
-      printf("Opção não conhecida.");
-      break;
+        break;
+      case 2:
+        system("clear");
+        if (carros==0){
+          printf("\nNão é possível abastecer pois não há nenhum carro na fila.\n");
+          } else {
+          if (carros>=1){
+            while (gasolina<=0) {
+            printf("Abastecer com quantos litros? OBS: Até 2 casas " 
+                   "decimais.");
+                scanf("%f", &gasolina);
+              }
+              if ((tanque-gasolina)>0){
+            vendasrel = gasolina*preco;
+            tanque -= gasolina;
+            atendrel = atendrel + 1;
+            gasorel = gasorel + gasolina;
+            carros -= 1;
+            gasolina=0;
+              } else {
+              if (tanque-gasolina<0){
+                printf("Não há gasolina o suficiente no tanque para essa"
+                       "quantidade");
+              }
+              }
+            }
+          }
+        break;
+      case 3:
+        break;
+      case 4:
+        system("clear");
+        opcao2= ' ';
+        while (opcao2 != 'F') {
+          printf("\nA - Quantidade de litros vendida.\n" 
+                 "B - Valor total arrecadado com as vendas.\n"
+                 "C - Quantidade de carros atendidos.\n" 
+                 "D - Quantidade de combustível restante no" 
+                 "tanque.\n" 
+                 "E - Gerar arquivo para impressão (com todas as"
+                 "informações de A,B,C e D.\n"
+                 "F - Voltar ao menu anterior.\n");
+          printf("\nPor favor escolha uma das opções. ");
+          scanf("\n%c", &opcao2);
+          switch (opcao2) {
+          case 'A':
+            system("clear");
+            printf("\nA quantidade de litros vendida é: %2.f\n", gasorel);
+            scanf("%f", &pausa);
+            system("clear");
+            break;
+          case 'B':
+            system("clear");
+            printf("\nValor total arrecadado: %2.f\n", vendasrel);
+            scanf("%f", &pausa);
+            system("clear");
+            break;
+          case 'C':
+            system("clear");
+            printf("\nQuantidade de carros atendida é: %d\n", atendrel);
+            scanf("%f", &pausa);
+            system("clear");
+            break;
+          case 'D':
+            system("clear");
+            printf("\nQuantidade de combustível restante no " 
+                   "tanque é: %2.f\n", tanque);
+            scanf("%f", &pausa);
+            system("clear");
+            break;
+          case 'E':
+            system("clear");
+            printf("\nArquivo para impressão:\n A : %.2f.\n B :" 
+                   "%2.f.\n C : %d.\n D : %2.f.\n", gasorel, vendasrel, atendrel, tanque);
+            scanf("%f", &pausa);
+            system("clear");
+            break;
+          case 'F':
+            system("clear");
+            printf("\nVoltando ao outro menu.\n");
+            break;
+          default:
+            printf("\n               " "Opção fora do escopo.\n");
+            break;
+          }
+        }
+        break;
+      case 5:
+        system("clear");
+        printf("Encerrando programa.");
+        exit(0);
+        break;
+      default:
+        printf("Opção não conhecida.");
+        break;
+      }
     }
+    return 0;
   }
-  return 0;
-}
