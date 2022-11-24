5f1ae5df32273349d4477ed07ad577fdaf0d0d79
bernard000 <118988204+bernard0001@users.noreply.github.com>
2022-11-24 14:53:08 +0000

    entrega 8 - modificação de algumas linhas de código

---
+++ header.h
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define RED "\e[0;31m"
+#define WHT "\e[0;37m"
+#define GRN "\e[0;32m"
+#define MAG "\e[0;35m"
+#define BLK "\e[0;30m"
+#define BLU "\e[0;34m"
+#define YEL "\e[0;33m"
+//Função de espera para pausas.
+void espera(){
+  printf(GRN"\nAperte enter para continuar...\n"WHT);
+  getchar();
+  }
+//Função de validação de variável.
+float teste(char *gol){
+  float teste1 = 0;
+  while (teste1<=0){
+  printf("%s%s", MAG, gol);
+    scanf("%f", &teste1);
+    }
+return(teste1);
+  }
+//Função de menu.
+void menu(int fila, float tanque, float preco){
+printf(MAG"\n                            =MENU=                           \n\n\n");
+  printf("Preço da gasolina:"GRN" %.2f\n\n"MAG, preco);
+  printf("Carros na fila:"GRN" %d\n\n"MAG, fila);
+  if(tanque>100){ 
+    printf("Gasolina disponível no tanque:"GRN" %.2f\n\n"MAG, tanque);
+  } else if (tanque>50){
+    printf("Gasolina disponível no tanque:"YEL" %.2f\n\n"MAG, tanque);
+  } else {
+    printf("Gasolina disponível no tanque:"RED" %.2f\n\n"MAG, tanque);
+    }
+  printf("("GRN"1"MAG") Adicionar um carro a fila\n"
+           "("GRN"2"MAG") Abastecimento\n"
+           "("GRN"3"MAG") Exibir carros na fila de espera\n"
+           "("GRN"4"MAG") Relatórios\n" 
+           "("GRN"5"MAG") Encerrar\n");
+    printf("\nPor favor, escolha uma das opções.\n"WHT);
+}
+//Função de adição de carros a fila.
+void addcarro(int *carros, int fila){
+        if (*carros == fila) {
+          printf(RED"\n                Tamanho de fila máximo atingido.\n"WHT);
+        } else {
+        printf(GRN"\n                  Carro adicionado na fila.\n"WHT);
+        *carros += 1;
+        }
+  }
+//Função de flush.
+void flush_in(){ 
+    int ch;
+    while( (ch = fgetc(stdin)) != EOF && ch != '\n' ){} 
+}
+struct Tcarro{
+  char placa[15];
+  char cor[15];
+  char modelo[15];
+  int ano;
+  float Gvalor;
+  float Glitro;
+};
--- main.c
+++ main.c
@@ -9,67 +9,7 @@
 #define BLK "\e[0;30m"
 #define BLU "\e[0;34m"
 #define YEL "\e[0;33m"
-//Função de espera para pausas.
-void espera(){
-  printf(GRN"\nAperte enter para continuar...\n"WHT);
-  getchar();
-  }
-//Função de espera para pausas que necessitam de 2 getchars.
-void espera2(){
-  printf(GRN"\nAperte enter para continuar...\n"WHT);
-  getchar();
-  getchar();
-  }
-//Função de validação de variável.
-float teste(char *gol){
-  float teste1 = 0;
-  while (teste1<=0){
-  printf("%s%s", MAG, gol);
-    scanf("%f", &teste1);
-    }
-return(teste1);
-  }
-//Função de menu.
-void menu(int fila, float tanque, float preco){
-printf(MAG"\n                            =MENU=                           \n\n\n");
-  printf("Preço da gasolina:"GRN" %.2f\n\n"MAG, preco);
-  printf("Carros na fila:"GRN" %d\n\n"MAG, fila);
-  if(tanque>100){ 
-    printf("Gasolina disponível no tanque:"GRN" %.2f\n\n"MAG, tanque);
-  } else if (tanque>50){
-    printf("Gasolina disponível no tanque:"YEL" %.2f\n\n"MAG, tanque);
-  } else {
-    printf("Gasolina disponível no tanque:"RED" %.2f\n\n"MAG, tanque);
-    }
-  printf("("GRN"1"MAG") Adicionar um carro a fila\n"
-           "("GRN"2"MAG") Abastecimento\n"
-           "("GRN"3"MAG") Exibir carros na fila de espera\n"
-           "("GRN"4"MAG") Relatórios\n" 
-           "("GRN"5"MAG") Encerrar\n");
-    printf("\nPor favor, escolha uma das opções.\n"WHT);
-}
-//Função de adição de carros a fila.
-void addcarro(int *carros, int fila){
-        if (*carros == fila) {
-          printf(RED"\n                Tamanho de fila máximo atingido.\n"WHT);
-        } else {
-        printf(GRN"\n                  Carro adicionado na fila.\n"WHT);
-        *carros += 1;
-        }
-  }
-//Função de flush.
-void flush_in(){ 
-    int ch;
-    while( (ch = fgetc(stdin)) != EOF && ch != '\n' ){} 
-}
-struct Tcarro{
-  char placa[15];
-  char cor[15];
-  char modelo[15];
-  int ano;
-  float Gvalor;
-  float Glitro;
-};
+
 int main(void) {
   int opcao = 0,atendimento=0, fila=0, carros=0, abastecimento, atendrel=0, opcaogas2= 0, contfila=0, tamanhofila=0,i=0, tamanhoatend=1;
   float preco=0, pausa, tanque=200, gasolina=0, gasorel=0, vendasrel=0;
@@ -128,10 +68,19 @@ int main(void) {
         if (fila==0){
           printf(RED"Não é possível abastecer pois não há nenhum carro na fila.\n"WHT);
           } else {
+          if (tanque==0){
+            printf(RED"Não há mais gasolina disponível no tanque, a fila será dispensada.\n");
+            flush_in();
+            espera();
+            system("clear");
+            printf(RED"                     Dispensando a fila...\n"WHT);
+            free(filacarro);
+            fila -= fila;
+            }
           if (fila>=1){
             while (gasolina<=0) {
             printf(MAG"Abastecer com quantos litros? OBS: Até 2 casas " 
-                   "decimais."WHT);
+                   "decimais.\n"WHT);
                 scanf("%f", &gasolina);
               system("clear");
               }
@@ -142,12 +91,9 @@ int main(void) {
             fila -= 1;
             tamanhoatend++;
             atendimentos=realloc(atendimentos,tamanhoatend * sizeof(struct Tcarro));
-              strcpy(atendimentos[atendrel].placa, filacarro[0].placa);
-                strcpy(atendimentos[atendrel].cor, filacarro[0].cor);
-                  strcpy(atendimentos[atendrel].modelo, filacarro[0].modelo);
-                    atendimentos[atendrel].ano = filacarro[0].ano;
-                      atendimentos[atendrel].Gvalor = (gasolina*preco);
-                        atendimentos[atendrel].Glitro = gasolina;
+              atendimentos[atendrel]=filacarro[0];
+                atendimentos[atendrel].Gvalor = (gasolina*preco);
+                  atendimentos[atendrel].Glitro = gasolina;
             atendrel ++;
             gasolina=0;    
                 printf(GRN"\n\n                     Reorganizando fila...\n"WHT);
@@ -156,33 +102,29 @@ int main(void) {
                   }
                 i-=1;
                 } else {
-              if (tanque == 0){
-                printf(RED"Não há mais gasolina no tanque"WHT);
-                }
                 if (tanque-gasolina<0){
-                printf(MAG"Não há gasolina o suficiente no tanque para "                            "essa quantidade,gostaria de abastecer com o que"
-                         " resta no tanque? "GRN"S "MAG"ou"RED" N"WHT);
+                              while ((opcaogas != 'S') && (opcaogas != 'N')){
+                  printf(MAG"Não há gasolina o suficiente no tanque para "                            "essa quantidade,gostaria de abastecer com o que"
+                         " resta no tanque? "GRN"S "MAG"ou"RED" N\n"WHT);
                   scanf("\n%c", &opcaogas);
-                opcaogas2=0;
+                                }
+                                opcaogas2=0;
                 while (opcaogas2 != 1){
                 switch(opcaogas){
                   case 'S':                   
                     vendasrel += (tanque*preco);
                     gasorel += tanque;
                     fila -= 1;
-                    tanque -= tanque;
                     opcaogas2 = 1;                    
                     tamanhoatend++;
                     atendimentos=realloc(atendimentos,tamanhoatend * sizeof(struct Tcarro));
-                    strcpy(atendimentos[atendrel].placa, filacarro[0].placa);
-                    strcpy(atendimentos[atendrel].cor, filacarro[0].cor);
-                    strcpy(atendimentos[atendrel].modelo, filacarro[0].modelo);
-                    atendimentos[atendrel].ano = filacarro[0].ano;
-                    atendimentos[atendrel].Gvalor = (gasolina*preco);
-                    atendimentos[atendrel].Glitro = gasolina;
+                      atendimentos[atendrel]=filacarro[0];
+                        atendimentos[atendrel].Gvalor = (tanque*preco);
+                          atendimentos[atendrel].Glitro = tanque;
+                    tanque -= tanque;
                     atendrel += 1;
                     gasolina = 0;
-                    printf(GRN"\n\n                     Reorganizando fila...\n"WHT);
+                    printf(GRN"\n\n                      Reorganizando fila...\n"WHT);
                 for (int k = 0; k<i; k++){
                 filacarro[k] = filacarro[k+1];
                   }
@@ -253,13 +195,15 @@ int main(void) {
           case 'A':
             system("clear");
             printf(MAG"\nA quantidade de litros vendida é: %2.f\n"WHT, gasorel);
-            espera2();
+            flush_in();
+            espera();
             system("clear");
             break;
           case 'B':
             system("clear");
             printf(MAG"\nValor total arrecadado: %2.f\n"WHT, vendasrel);
-            espera2();
+            flush_in();
+            espera();
             system("clear");
             break;
           case 'C':
@@ -275,32 +219,36 @@ int main(void) {
                      printf(MAG"Valor gasto:"GRN" %.2f\n", atendimentos[k].Gvalor);
                       printf(MAG"Gasolina comprada em litros:"GRN" %.2f\n", atendimentos[k].Glitro);
           }  
-          espera2();
+            flush_in();
+            espera();
             system("clear");
             break;
           case 'D':
             system("clear");
             printf(MAG"\nQuantidade de combustível restante no" 
                    " tanque é: %2.f\n"WHT, tanque);
-            espera2();
+            flush_in();
+            espera();
             system("clear");
             break;
           case 'E':
             system("clear");
             printf(MAG"\nArquivo para impressão:\n A : %.2f.\n B :" 
                    "%2.f.\n C : %d.\n D : %2.f.\n"WHT, gasorel, vendasrel, atendrel, tanque);
-            espera2();
+            flush_in();
+            espera();
             system("clear");
             break;
           case 'F':
             system("clear");
             printf(GRN"\nVoltando ao outro menu.\n"WHT);
-            espera2();
+            flush_in();
+            espera();
             system("clear");
             break;
           default:
             system("clear");
-            printf(RED"\n               " "Opção fora do escopo.\n"WHT);
+            printf(RED"\n                   Opção fora do escopo.\n"WHT);
             break;
           }
         }
@@ -311,7 +259,7 @@ int main(void) {
         exit(0);
         break;
       default:
-        printf(RED"                     Opção não conhecida.\n"WHT);
+        printf(RED"                   Opção não conhecida.\n"WHT);
         getchar();
         system("clear");
         break;
